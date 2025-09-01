49c641044101cb2d0e02bd258f0d608ed3677467
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-01 00:09:26 -0300

    version 1.2.9 - create lib standardization-units standardization names to glpi

---
+++ src/lib/standardization-units.ts
@@ -0,0 +1,9 @@
+const standardizationUnits = {
+  apura: 'UBS/ESF Jardim Apurá',
+  chacarasto: 'UBS Chácara Santo',
+  Guacuri: 'UBS/ESF Vila Guacuri',
+  mpaulista: 'UBS/ESF Mar Paulista',
+  pantanal: 'Jardim Novo Pantanal'
+}
+
+export default standardizationUnits
