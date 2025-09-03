93b7776a4aadfea1e394f5a553a9b7d3acd9f61a
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-03 20:22:56 -0300

    version 1.3.12

--- src/application/glpi-create-called.ts
+++ src/application/glpi-create-called.ts
@@ -37,26 +37,35 @@ export class GlpiCreateCalled {
         # campo titulo
           document.querySelector("#mainformtable4 input").value = "ola"
         # Descrição
+          obs: iframeElement
           document.querySelector("#tinymce").innerText = "osdfsdf"
+          $$$$$$$$$$$$$$$$$
+              const iframe = document.querySelector('iframe[id^="content"]')
+              const iframeDoc = iframe.contentDocument || iframe.contentWindow.document
+              const p = iframeDoc.querySelector('p')
+              if (p) {
+                p.textContent = "Texto alterado direto no Chrome 🚀"
+              }
+          $$$$$$$$$$$$$$$$$$
         # botão Adicionar
           document.querySelector(".submit").click()
 
         #######################################################
 
         # Tipo Requisição
-          dropdown_type286619369
           document.querySelectorAll(".select2-hidden-accessible")[3].value = "2"
           document.querySelectorAll(".select2-hidden-accessible")[3].onchange()
         # Categoria
-          dropdown_itilcategories_id2026503822
           document.querySelectorAll(".select2-hidden-accessible")[4].innerHTML = '<option value=""1030 selected="selected">BACKUP > Acompanhamento Diario Rotina de Backup</option>'
 
         #######################################################
 
         # Requerente GROUP
-          document.querySelector("#dropdown__groups_id_requester1829935631").innerHTML = '<option value="170" selected="selected">UBS/ESF Mar Paulista</option>'
+          document.querySelectorAll(".select2-hidden-accessible")[8].innerHTML = '<option value="170" selected="selected">UBS/ESF Mar Paulista</option>'
         # Observador GROUP
-          document.querySelector("#dropdown__groups_id_observer153146980").innerHTML = '<option value="170" selected="selected">UBS/ESF Mar Paulista</option>'
+          document.querySelectorAll(".select2-hidden-accessible")[11].innerHTML = '<option value="170" selected="selected">UBS/ESF Mar Paulista</option>'
+        # Atribuído para GROUP
+          document.querySelectorAll(".select2-hidden-accessible")[14].innerHTML = '<option value="145" selected="selected">Infraestrutura T.I</option>'
 
     */
     
