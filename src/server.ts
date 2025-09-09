72e034a0fddafc89e903956e7c8640695dbbfe8b
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-09 10:41:27 -0300

    version 1.4.16 - server.ts: fixing multiple WebSocketServer to work

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,21 @@ import { WebSocketServer } from "ws";
 
 const server = app.listen(env.PORT, () => console.log("Server in running port " + env.PORT))
 
-const wss1 = new WebSocketServer({ server, path: '/ws1' })
-const wss2 = new WebSocketServer({ server, path: '/ws2' })
+const wss1 = new WebSocketServer({ noServer: true })
+const wss2 = new WebSocketServer({ noServer: true })
+
+server.on("upgrade", (req, socket, head) => {
+  if (req.url === "/ws1") {
+    wss1.handleUpgrade(req, socket, head, (ws) => {
+      wss1.emit("connection", ws, req)
+    })
+  } else if (req.url === "/ws2") {
+    wss2.handleUpgrade(req, socket, head, (ws) => {
+      wss2.emit("connection", ws, req)
+    })
+  } else {
+    socket.destroy()
+  }
+})
 
 export { wss1, wss2 }
